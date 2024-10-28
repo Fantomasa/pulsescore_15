4f5d5bd1e93df21a86db10b09c90b1333b93c7fa
Fantomasa <ivailoinfo@gmail.com>
2024-10-28 16:00:16 +0200

    update

---
+++ src/components/Header.tsx
@@ -0,0 +1,22 @@
+import { AudioLines } from "lucide-react";
+import Link from "next/link";
+import Form from "next/form";
+
+export default async function Header() {
+  return (
+    <header className="flex justify-between items-center gap-3 p-4">
+      <Link href="/" className="flex gap-1 items-center">
+        <AudioLines className="w-6 h-6 text-primary" />
+        <p className="text-2xl font-bold">PulseScore</p>
+      </Link>
+      <Form action="/" scroll={false} className="flex items-center bg-foreground rounded-lg w-1/2">
+        <input
+          name="query"
+          placeholder="Search Team"
+          className="w-full border-white border-[1px] outline-white text-white bg-foreground p-2 rounded-md"
+        />
+      </Form>
+      <div>Login</div>
+    </header>
+  );
+}
