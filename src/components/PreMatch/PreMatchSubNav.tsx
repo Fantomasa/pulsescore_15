efd70315568954a0a9158d88e548d6041f7eece5
Fantomasa <ivailoinfo@gmail.com>
2024-10-30 10:17:02 +0200

    SubNav

--- src/components/PreMatch/PreMatchSubNav.tsx
+++ src/components/PreMatch/PreMatchSubNav.tsx
@@ -2,37 +2,35 @@
 
 import Link from "next/link";
 import { useParams, usePathname } from "next/navigation";
-import { Badge } from "../ui/badge";
-
-const activeClassName = "bg-primary text-white hover:text-black hover:bg-white";
 
 export default function PreMatchSubNav() {
   const params = useParams<{ seasonId: string }>();
   const seasonId = params.seasonId;
 
   const pathname = usePathname();
+  console.log({ pathname });
 
   return (
     <nav>
       <ul className="flex w-full gap-20 justify-center mt-5 px-10 mb-5">
         <li>
-          <Link href={`/pre-match/${seasonId}/table`}>
-            <Badge
-              className={`text-center mx-auto justify-center text-sm ${pathname.includes("table") ? activeClassName : ""}`}
-              variant={"secondary"}
-            >
-              Table
-            </Badge>
+          <Link
+            href={`/pre-match/${seasonId}/table`}
+            className={`text-center mx-auto justify-center text-sm hover:underline underline-offset-4 ${
+              pathname.endsWith("table") ? "underline" : ""
+            }`}
+          >
+            Table
           </Link>
         </li>
         <li>
-          <Link href={`/pre-match/${seasonId}/fixtures`}>
-            <Badge
-              className={`text-center mx-auto justify-center text-sm ${pathname.includes("fixtures") ? activeClassName : ""}`}
-              variant={"secondary"}
-            >
-              Fixtures
-            </Badge>
+          <Link
+            href={`/pre-match/${seasonId}/fixtures`}
+            className={`text-center mx-auto justify-center text-sm hover:underline underline-offset-4 ${
+              pathname.endsWith("fixtures") ? "underline" : ""
+            }`}
+          >
+            Fixtures
           </Link>
         </li>
       </ul>
