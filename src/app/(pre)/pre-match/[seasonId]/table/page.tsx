98fa781ca04377e0658199e194b13d2d4fadee64
Fantomasa <ivailoinfo@gmail.com>
2024-10-30 12:17:29 +0200

    framer-motion added

--- src/app/(pre)/pre-match/[seasonId]/table/page.tsx
+++ src/app/(pre)/pre-match/[seasonId]/table/page.tsx
@@ -1,9 +1,16 @@
 import TableComponent from "@/components/PreMatch/Table/Table";
+import Transition from "@/components/Transition";
 import { getTable } from "@/services/pre-match/pre-match";
 
 export default async function TablePage({ params }: { params: Promise<{ seasonId: string }> }) {
   const seasonId = (await params).seasonId;
   const tableResult = await getTable(seasonId);
 
-  return <TableComponent tableRows={tableResult.data[0].tablerows} />;
+  if (tableResult.data.length <= 0) return <div className="flex text-2xl mx-auto justify-center">Sorry no information found</div>;
+
+  return (
+    <Transition>
+      <TableComponent tableRows={tableResult.data[0].tablerows} />;
+    </Transition>
+  );
 }
