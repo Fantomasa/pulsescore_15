90f75ca29ef6a1f1d0b19a35701ef4fad476bf90
Fantomasa <ivailoinfo@gmail.com>
2024-10-31 13:23:13 +0200

    Fix Header

--- src/components/common/NavLink.tsx
+++ src/components/common/NavLink.tsx
@@ -7,8 +7,8 @@ const acitveClass = "text-primary bg-secondary";
 export default function NavLink({ href, currentPath, children }: { href: string; currentPath: string; children: React.ReactNode }) {
   return (
     <Link
-      className={`p-2 bg-foreground rounded-md items-center border hover:text-primary hover:bg-secondary ${
-        href.includes(currentPath) ? acitveClass : ""
+      className={`w-[60px] md:w-[80] md:text-sm text-xs text-center p-2 bg-foreground rounded-md items-center border hover:text-primary hover:bg-secondary ${
+        href.includes(currentPath) && currentPath !== "/" ? acitveClass : ""
       }`}
       href={href}
     >
