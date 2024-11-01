442bb934a006e81f96df840366335baf88cdeb2b
Fantomasa <ivailoinfo@gmail.com>
2024-11-01 14:41:24 +0200

    home-page

--- src/components/common/NavLink.tsx
+++ src/components/common/NavLink.tsx
@@ -7,7 +7,7 @@ const acitveClass = "text-primary bg-secondary";
 export default function NavLink({ href, currentPath, children }: { href: string; currentPath: string; children: React.ReactNode }) {
   return (
     <Link
-      className={`w-[80] md:text-sm text-xs text-center p-2 bg-secondary rounded-md items-center border hover:text-primary hover:bg-secondary ${
+      className={`w-[80] md:text-sm text-xs text-center p-2 bg-secondary rounded-md items-center border md:hover:text-primary md:hover:bg-secondary ${
         href.includes(currentPath) && currentPath !== "/" ? acitveClass : ""
       }`}
       href={href}
