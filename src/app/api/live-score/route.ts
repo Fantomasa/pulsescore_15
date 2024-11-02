483c8f84383cd0600f11cdfd53b7c7070d7f9b93
Fantomasa <ivailoinfo@gmail.com>
2024-11-02 08:49:37 +0200

    add live-score route

---
+++ src/app/api/live-score/route.ts
@@ -0,0 +1,13 @@
+import { getLiveEvents } from "@/services/live-score/live-score";
+import { NextResponse, NextRequest } from "next/server";
+
+export async function GET(request: NextRequest) {
+  const searchParams = request.nextUrl.searchParams;
+  let sport = searchParams.get("sport");
+
+  if (!sport) sport = "football";
+
+  const data = await getLiveEvents(sport);
+
+  return NextResponse.json(data);
+}
