f5c29e7324447f4b0d1f6370d948a9c15937efe2
Fantomasa <ivailoinfo@gmail.com>
2024-11-02 09:27:54 +0200

    add spinner on loading

--- src/components/Live/LiveScore.tsx
+++ src/components/Live/LiveScore.tsx
@@ -10,17 +10,22 @@ import { LiveEventsResult } from "@/services/live-score/schemas";
 import Spinner from "../common/Spinner";
 
 export default function LiveScoreComponent({ sport }: { sport: string }) {
-  const { data: liveData, isLoading } = useQuery<LiveEventsResult>({
+  const {
+    data: liveData,
+    isLoading,
+    isError
+  } = useQuery<LiveEventsResult>({
     queryKey: ["live-score", sport],
     queryFn: () => fetch(`/api/live-score?sport=${sport}`).then((res) => res.json()),
     refetchInterval: 5000
   });
 
-  // console.log({ liveData });
+  if (isLoading) return;
+  <div className="w-full mx-auto">
+    <Spinner />
+  </div>;
 
-  if (isLoading) return <Spinner />;
-
-  if (!liveData || (liveData && liveData?.total <= 0)) {
+  if (isError || !liveData || (liveData && liveData?.total <= 0)) {
     return (
       <div className="flex gap-2 items-center text-center justify-center">
         No avaible events for <p className="font-bold text-xl uppercase">{sport}</p>
